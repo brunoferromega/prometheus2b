97a0db4e3818f5c8a83a631bef6cb3c7a2038e57
brunoferromega <brunoferromega@outlook.com>
2025-01-02 15:04:50 -0300

    feat/: implement get authors

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 
 const PORT = ":8080"
 
+var db *memdb.MemDB
+
 type Article struct {
 	Id      uint   `json:"id"`
 	Title   string `json:"title"`
@@ -25,17 +27,13 @@ type Author struct {
 }
 
 func (a *Author) SaveDB(db *memdb.MemDB) (err error) {
-	txn := *db.Txn(true)
-	if err := txn.Insert("author", a); err != nil {
-		return err
-	}
-	txn.Commit()
 	return nil
 }
 
 func main() {
+	var err error
 	schema := buildSchema()
-	db, err := memdb.NewMemDB(schema)
+	db, err = memdb.NewMemDB(schema)
 	if err != nil {
 		log.Panicln("Error on starting schema db...", err)
 	}
@@ -44,35 +42,70 @@ func main() {
 
 	fmt.Println("Server starting on port 8080...")
 
-	mux.HandleFunc("GET /healthcheck", aliveChecker)
-
-	mux.HandleFunc("POST /authors", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading body", http.StatusBadRequest)
-			return
-		}
-
-		var author Author
-		if err := json.Unmarshal(body, &author); err != nil {
-			http.Error(w, "Error unmarshaling body", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Println(author)
-		author.SaveDB(db)
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(author)
+	mux.HandleFunc("GET /healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Current I'm alive\n")
 	})
+
+	mux.HandleFunc("POST /authors", createAuthor)
+	mux.HandleFunc("GET /authors", getAllAuthors)
 
 	if err := http.ListenAndServe(PORT, mux); err != nil {
 		log.Panicln("Error on starting server...", err)
 	}
 }
 
-func aliveChecker(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Current I'm alive\n")
+func getAllAuthors(w http.ResponseWriter, r *http.Request) {
+	txn := *db.Txn(false)
+	defer txn.Abort()
+
+	it, err := txn.Get("author", "id")
+	if err != nil {
+		http.Error(w, "Error when was being: Reading Authors From Database", http.StatusInternalServerError)
+        return
+	}
+
+    var authors []Author
+    for obj := it.Next(); obj != nil; obj = it.Next() {
+        author := obj.(*Author)
+        authors = append(authors, *author)
+    }
+
+    fmt.Println(authors)
+
+    jsonData, err := json.Marshal(authors)
+    if err != nil {
+		http.Error(w, "Error when was being: Unmarshaling Body", http.StatusInternalServerError)
+		return
+    }
+    
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(string(jsonData))
+}
+
+func createAuthor(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error when was being: Reading Body", http.StatusBadRequest)
+		return
+	}
+
+	var author Author
+	if err := json.Unmarshal(body, &author); err != nil {
+		http.Error(w, "Error when was being: Unmarshaling Body", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(author)
+
+	txn := *db.Txn(true)
+	if err := txn.Insert("author", &author); err != nil {
+		http.Error(w, "Error when was being: Saving Author in Database", http.StatusInternalServerError)
+		return
+	}
+	txn.Commit()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(author)
 }
 
 func buildSchema() *memdb.DBSchema {
